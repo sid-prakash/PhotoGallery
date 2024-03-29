6bcb6432a217e45d9c20b30d97603b8ecb158fe0
brade <bradenbuckalew@gmail.com>
2024-03-29 00:53:20 -0500

    updates for download

--- flaskr/gallery.py
+++ flaskr/gallery.py
@@ -1,6 +1,9 @@
+import os
+import tempfile
+
 from boto3.dynamodb.conditions import Key, Attr
 from flask import (
-    Blueprint, current_app,flash, g, redirect, render_template, request, url_for
+    Blueprint, current_app, flash, g, redirect, render_template, request, url_for, send_from_directory, send_file
 )
 from werkzeug.exceptions import abort
 from werkzeug.utils import secure_filename
@@ -46,3 +49,15 @@ def upload_file():
     
 
     return render_template('fileupload/gallery.html', image=photo_url, as_attachment=True)
+
+@bp.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    # Download file from S3 bucket to a temporary file
+    s3 = boto3.client('s3', aws_access_key_id=current_app.config['S3_KEY'], aws_secret_access_key=current_app.config['S3_SECRET'])
+
+    key = os.path.basename(filename)
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    s3.download_fileobj( current_app.config['S3_BUCKET'], key, temp_file)
+
+    # Serve the downloaded file
+    return send_file(temp_file.name, as_attachment=True, download_name=os.path.basename(filename))
